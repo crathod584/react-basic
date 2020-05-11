8b900c4215c1574833ab9d43eb6311f34826f916
northout <crathod584@gmail.com>
2020-05-11 20:33:37 +0530

    react first project with element

--- src/index.js
+++ src/index.js
@@ -1,17 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './index.css'
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const element = React.createElement('div',{className:'demoClass'},
+    React.createElement('h1',null,'CHetan'),
+    React.createElement('h1',null,'Tutorials'),
+ 
 );
+ReactDOM.render(element,document.getElementById('root'));
+
+// const element  = React.createElement('h1',null,'chetan');
+
+// ReactDOM.render(element,document.getElementById('root'));
+
+
+// const element = (
+//      <div className="demoClass">
+//     <h1 >chetan</h1>
+//     <h1 >Tutorials</h1>
+//     </div>
+//     );
+
+//     ReactDOM.render(element,document.getElementById('root')); 
+
+
+// const element = <h1 className="demoClass">chetan</h1>
+// ReactDOM.render(element,document.getElementById('root'));
+
+// const newelement = <h1 className="demoClass">Tutorials</h1>
+// ReactDOM.render(newelement,document.getElementById('abc'));
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+// Learn more about service worekers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
