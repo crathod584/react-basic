ecaa91aa954381c7a71fe649e9360dfaf38c5b6b
northout <crathod584@gmail.com>
2020-05-11 22:16:14 +0530

    funtion component with data pass

--- src/index.js
+++ src/index.js
@@ -3,33 +3,61 @@ import ReactDOM from 'react-dom';
 import './index.css'
 import * as serviceWorker from './serviceWorker';
 
-const element = React.createElement('div',{className:'demoClass'},
-    React.createElement('h1',null,'CHetan'),
-    React.createElement('h1',null,'Tutorials'),
- 
-);
-ReactDOM.render(element,document.getElementById('root'));
+//----------------Calling arrow funtion with props (or passing valu in component) using varible
+ const StudentDetails = (student) =>{
+  return <div>
+   
+   <p><label>Student ID: <strong>{student.ID}</strong></label></p>
+   <p><label>Student Name: <strong>{student.Name}</strong></label></p>
+     </div>  
+ }
+ReactDOM.render(<StudentDetails ID="121" Name="Chetan"/>,document.getElementById('root'));
 
-// const element  = React.createElement('h1',null,'chetan');
 
+//----------------Calling funtion with props (or passing valu in component) using varible
+// function StudentDetails(student){
+//   return <div>
+   
+// <p><label>Student ID: <strong>{student.ID}</strong></label></p>
+// <p><label>Student Name: <strong>{student.Name}</strong></label></p>
+//   </div>  
+// }
+// const element = <StudentDetails ID="12" Name="Chetan"/>;
 // ReactDOM.render(element,document.getElementById('root'));
 
 
-// const element = (
-//      <div className="demoClass">
-//     <h1 >chetan</h1>
-//     <h1 >Tutorials</h1>
-//     </div>
-//     );
 
-//     ReactDOM.render(element,document.getElementById('root')); 
+//----------------Calling funtion with props (or passing valu in component)
+// function StudentDetails(student){
+//   return <div>
+   
+// <p><label>Student ID: <strong>{student.ID}</strong></label></p>
+// <p><label>Student Name: <strong>{student.Name}</strong></label></p>
+//   </div>  
+// }
+// ReactDOM.render(<StudentDetails ID="1" Name="Chetan"/>,document.getElementById('root'));
+
+// -------------------------Caling arrow funtion as component in ReactDOM.render method
+// const Greet = () => {
+//   return <h1>CHetan-3</h1>
+// }
+// ReactDOM.render(<Greet/>,document.getElementById('root'));
 
 
-// const element = <h1 className="demoClass">chetan</h1>
-// ReactDOM.render(element,document.getElementById('root'));
 
-// const newelement = <h1 className="demoClass">Tutorials</h1>
-// ReactDOM.render(newelement,document.getElementById('abc'));
+// ----------------------------------Caling funtion as component in ReactDOM.render method
+// function Greet(){
+//   return <h1>CHetan-1</h1>
+// }
+// ReactDOM.render(<Greet/>,document.getElementById('root'));
+
+
+
+// ------------------------------------Caling funtion in ReactDOM.render method
+// function Greet(){
+//   return <h1>CHetan-1</h1>
+// }
+// ReactDOM.render(Greet(),document.getElementById('root'));
 
 
 // If you want your app to work offline and load faster, you can change
